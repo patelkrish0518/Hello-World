4d1f6b9ba2347923047932f85179fb2bbbdb7856
patelkrish0518 <65685486+patelkrish0518@users.noreply.github.com>
2020-08-12 17:48:39 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Krish!");
+		System.out.println("I like playing Soccer and video games.");
+		System.out.println("My favorite Mobile Video game currently is Brawl Stars.");
+		System.out.println("My favorite PC video game is Star Wars: The Fallen Order.")
+		System.out.println(" I also like programming so Computer Science is a possible major which matches my interest.");
+	}
+}
